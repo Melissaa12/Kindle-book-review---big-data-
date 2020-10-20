7996e771064041b6ec1611786c0743df3e77f47c
jazreel <jazreel.kwek@gmail.com>
2020-10-20 12:13:56 +0800

    front end not complete

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index_page_landing():
+    return render_template('index.html')
+@app.route('/book')
+def hello_world():
+   return render_template('book.html')
+
+if __name__ == "__main__":
+    app.run()
