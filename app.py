99549fdd888b5d423e16df983d41b3783993a513
Ryan <ryangen97@gmail.com>
2020-12-04 08:43:49 +0800

    -added mel stuff with varsha

--- app.py
+++ app.py
@@ -4,13 +4,53 @@ import json
 import jinja2
 from data import GetBookDetails
 from reviews import GetReviewData
-
+import numpy as np
+import pandas as pd
+import math 
 import sys
+from pyspark import SparkContext
+from pyspark import SparkConf
+from pyspark.sql.session import SparkSession
+from pyspark.ml.feature import CountVectorizer, IDF, Tokenizer
+from pyspark.sql.functions import udf
+from pyspark.mllib.feature import HashingTF
 
 print('This is the mongo public IP Address' +  sys.argv[2])
 mongopublicip = sys.argv[1]
 mongopublicipadd = 'http://' + mongopublicip + '/asin/'
+from pyspark.ml.feature import CountVectorizer, IDF, Tokenizer
+from pyspark.sql.functions import udf, col
+from pyspark.sql.types import StringType
+from pyspark.mllib.feature import HashingTF
+from pyspark.sql.functions import *
+from pyspark.sql.functions import length
+conf=SparkConf()
+conf.set("spark.driver.memory", "5g")
+sc = SparkContext.getOrCreate(conf)
+masternodeip = sys.argv[3]
 
+sc.setCheckpointDir("hdfs://"+masternodeip+':9000:'+"/project")
+spark = SparkSession(sc)
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index_page_landing():
+    return render_template('index.html')
+@app.route('/book')
+def hello_world():
+   return render_template('book.html')
+@app.route('/add-review')
+def add_review():
+   return render_template('addReview.html')
+@app.route('/correlation')
+def correlation():
+   return render_template('correlation.html')
+@app.route('/tf-idf')
+def tf_idf():
+   return render_template('tf-idf.html')
+def string_length(reviewText):
+    x = len(reviewText)
+    return x
 print('This is the sql public IP Address' +  sys.argv[1])
 sqlpublicip = sys.argv[2]
 
@@ -27,7 +67,61 @@ def index_page_landing():
     return render_template('index.html',reviews=reviews)
     #return render_template('index3.html')
 
+@app.route('/predict', methods=['POST'])
+def search():
+   #  ,asin,helpful,overall,reviewText,reviewTime,reviewerID,reviewerName,summary,unixReviewTime
 
+    wordR = request.form['tfidfword']
+    data = spark.read.csv("hdfs://"+masternodeip+':9000'+"/project/kindle_reviews.csv", header=True, sep=",")
+    data = data.na.drop(subset=["reviewText"])
+   
+    tokenizer = Tokenizer(inputCol="reviewText",outputCol="words")
+    wordsData = tokenizer.transform(data)
+
+    cv = CountVectorizer(inputCol="words", outputCol="rawFeatures",vocabSize = 2000)
+    model = cv.fit(wordsData)
+    featurizedData = model.transform(wordsData)
+    vocab = model.vocabulary
+    idf = IDF(inputCol= "rawFeatures", outputCol="features")
+    idfModel = idf.fit(featurizedData)
+    rescaledData = idfModel.transform(featurizedData)
+    tfidfRow=[]
+    #get tfidf of each row
+    def searchFunc (row,vocab,req):
+       a = {}
+       index = vocab.index(req)
+       array = row.toArray()
+       tfidfWord = 0
+       for i in range(len(row)):
+         if (array[i]!=0  ):
+             tfidf=array[i]
+             word= vocab[i]
+             if(word==req):
+               # print(tfidf)
+               tfidfWord = tfidf
+               a[word]= tfidf
+       tfidfRow.append(tfidfWord)
+       return str(a)
+    def map_to_word1(row, vocab):
+      d = {}
+      array = row.toArray()
+      # print(array[0])
+      # print(vocab[0])
+      # print(array[1])
+      # print(vocab[1])
+      for i in range(len(row)):
+         if (array[i] != 0):
+               tfidf = array[i]
+               word = vocab[i]
+               d[word] = tfidf
+      return str(d)
+    def map_to_word(vocab):
+      # return udf(lambda row: map_to_word1(row, vocab))
+      return udf(lambda row: searchFunc(row, vocab, wordR))
+    output0 = rescaledData.withColumn("features", map_to_word(vocab)(rescaledData.features))
+    output = output0.select("asin","features")
+    output.write.format("csv").save("hdfs://"+masternodeip+':9000'+"/result2")
+    return render_template('tfidfresult.html', data="placeholder")
 @app.route('/book/<string:asin>')
 def hello_world(asin):
    #api.add_resource(BookDetail,'/book/<string:asin>')
@@ -74,4 +168,4 @@ def add_review():
     return render_template('addReview.html') 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
