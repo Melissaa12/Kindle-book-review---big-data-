88f0ec31c9e04e0f1c7bcbc4bfd64e7930cefa6d
Ryan <ryangen97@gmail.com>
2020-12-06 09:05:07 +0800

    cahnge dependencies 905

--- SQLWITHPROFINS/dependencies.sh
+++ SQLWITHPROFINS/dependencies.sh
@@ -1,6 +1,6 @@
 yes Y|sudo apt install python3-venv
 sudo python3 -m venv venv --without-pip --system-site-packages
+export LC_ALL=C
  . venv/bin/activate
 pip3 install boto3
 yes Y|sudo apt install python3-pip
-export LC_ALL=C
