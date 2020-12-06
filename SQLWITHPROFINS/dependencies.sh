9a3cfb705bbefc250d27cce27053e962276dd915
Ryan <ryangen97@gmail.com>
2020-12-06 09:25:42 +0800

    fix925

--- SQLWITHPROFINS/dependencies.sh
+++ SQLWITHPROFINS/dependencies.sh
@@ -1,6 +1,10 @@
 yes Y|sudo apt install python3-venv
+
 sudo python3 -m venv venv --without-pip --system-site-packages
 export LC_ALL=C
  . venv/bin/activate
+yes Y|sudo apt install python-pip3
 pip3 install boto3
-yes Y|sudo apt install python3-pip
+pip3 install pyyaml
+pip3 install --upgrade pip
+pip3 install paramiko
